90690f579b07fa67bb03cfa11fae0f739ec9ebb0
locphu95 <33072788+locphu95@users.noreply.github.com>
2018-07-02 18:29:51 +0700

    Update LoaigvController.cs

--- Controllers/LoaigvController.cs
+++ Controllers/LoaigvController.cs
@@ -8,24 +8,17 @@ namespace NguyenPhuLoc.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
-    public class LoaigvController : Controller
+    public class LoaigiangvienController : Controller
     {
         private AppDbContext _db;
-        public LoaigvController(AppDbContext db)
+        public LoaigiangvienController(AppDbContext db)
         {
             _db = db;
         }
-        [HttpGet("{manv}")]
-        public IActionResult Get(string manv)
-        {
-            var model = _db.LoaiGiangVien.Where(x => x.MaLoaiGiangVien == manv).ToList();
-            return Ok(model);
-        }
-
         [HttpGet]
         public IActionResult Get()
         {
-            var model = _db.LoaiGiangVien.ToList();
+            var model = from a in  _db.LoaiGiangVien where a.TrangThai==true select a;
             return Ok(model);
         }
         [HttpGet("{id}")]
@@ -60,6 +53,54 @@ namespace NguyenPhuLoc.Controllers
             {
                 return BadRequest();
             }
+        }
+        [HttpPost("Edit/{id}")]
+         public IActionResult Post2([FromBody] LoaiGiangVien cb,string id)
+        {
+          try{
+            LoaiGiangVien edit =_db.LoaiGiangVien.FirstOrDefault(x=>x.MaLoaiGiangVien==id);       
+              edit.TenLoaiGiangVien=cb.TenLoaiGiangVien;
+              edit.BaoHiem=cb.BaoHiem;
+            _db.SaveChanges();
+            return Ok("Them thanh cong");
+          }
+          catch{
+             return BadRequest();
+          }
+        }
+        [HttpGet("Delete/{id}")]
+         public IActionResult Post3(string id)
+        {
+          try{
+            LoaiGiangVien del =_db.LoaiGiangVien.FirstOrDefault(x=>x.MaLoaiGiangVien==id);
+             del.TrangThai = false;
+            _db.SaveChanges();
+            return Ok("Xoa thanh cong");
+          }
+          catch{
+             return BadRequest();
+          }
+        }
+        [HttpPost]
+      public IActionResult Post([FromBody] LoaiGiangVien cb)
+        {
+          cb.TrangThai=true;
+          try
+          {
+            LoaiGiangVien tour=new LoaiGiangVien(){
+                MaLoaiGiangVien=cb.MaLoaiGiangVien,
+                TenLoaiGiangVien=cb.TenLoaiGiangVien,
+                BaoHiem= cb.BaoHiem,
+                TrangThai=cb.TrangThai
+            };
+            _db.LoaiGiangVien.Add(tour);
+            _db.SaveChanges();
+            return Ok("Them thanh cong");
+          }
+          catch (Exception)
+          {
+            return BadRequest();
+          }
         }
 
 
