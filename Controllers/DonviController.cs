e896ceede4b02da102a46a28eeac6ae9e28fd627
locphu95 <33072788+locphu95@users.noreply.github.com>
2018-07-06 17:42:05 +0700

    6/7 don vi

--- Controllers/DonviController.cs
+++ Controllers/DonviController.cs
@@ -15,55 +15,86 @@ namespace NguyenPhuLoc.Controllers
         {
             _db = db;
         }
-        [HttpGet("{manv}")]
-        public IActionResult Get(string manv)
-        {
-            var model = _db.DonVi.Where(x => x.MaDonVi == manv).ToList();
-            return Ok(model);
-        }
-
         [HttpGet]
         public IActionResult Get()
         {
-            var model = _db.DonVi.ToList();
+            var model =from a in _db.DonVi where  a.TrangThai==true select a;
             return Ok(model);
         }
         [HttpGet("{id}")]
-        public IActionResult Getcv(string id)
+        public IActionResult GetNH(string id)
         {
-            try
-            {
-                DonVi model = _db.DonVi.Find(id);
-                return Ok(model);
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
+            var model= _db.DonVi.Find(id);
+            return Ok(model);
         }
         [HttpGet("Detail/{id}")]
-        public IActionResult GetDetail(int id)
+        public IActionResult GetDetail(string id)
         {
             try
             {
-                var model = (from cb in _db.CBNV
-                             from l in _db.LoaiGiangVien
-                             where cb.MaLoaiGiangVien == l.MaLoaiGiangVien
+                var model = (from nh in _db.NamHoc 
+                             join hk in _db.HocKy on nh.MaNamHoc equals hk.MaNamHoc
+                             where hk.MaHocKy==id 
                              select new
                              {
-                                 l.TenLoaiGiangVien,
-
+                                nh.MaNamHoc,nh.NgayBatDauNamHoc,nh.NgayKetThucNamHoc,nh.MoTaNamHoc,hk.MaHocKy
                              }).ToList();
-                return Ok(model);
+                var kq = from a in model group a by new{ a.MaNamHoc,a.NgayBatDauNamHoc,a.NgayKetThucNamHoc,a.MoTaNamHoc,a.MaHocKy} into g select new { g.Key.MaNamHoc,g.Key.NgayBatDauNamHoc,g.Key.NgayKetThucNamHoc,g.Key.MoTaNamHoc,g.Key.MaHocKy};
+                return Ok(kq);
             }
             catch (Exception)
             {
                 return BadRequest();
             }
         }
-
-
-
+        [HttpPost]
+        public IActionResult Post([FromBody] DonVi cb)
+        {
+          cb.TrangThai=true;
+          try
+          {
+            DonVi tour=new DonVi(){
+                MaDonVi=cb.MaDonVi,
+                TenDonVi=cb.TenDonVi,
+                DienThoai= cb.DienThoai,
+                TrangThai=cb.TrangThai
+            };
+            _db.DonVi.Add(tour);
+            _db.SaveChanges();
+            return Ok("Them thanh cong");
+          }
+          catch (Exception)
+          {
+            return BadRequest();
+          }
+        }
+        [HttpGet("Delete/{id}")]
+         public IActionResult Post3(string id)
+        {
+          try{
+            DonVi del =_db.DonVi.FirstOrDefault(x=>x.MaDonVi==id);
+             del.TrangThai = false;
+            _db.SaveChanges();
+            return Ok("Xoa thanh cong");
+          }
+          catch{
+             return BadRequest();
+          }
+        }
+        [HttpPost("Edit/{id}")]
+         public IActionResult Post2([FromBody] DonVi cb,string id)
+        {
+          try{
+            DonVi edit =_db.DonVi.FirstOrDefault(x=>x.MaDonVi==id);
+              edit.TenDonVi=cb.TenDonVi;
+              edit.DienThoai=cb.DienThoai;
+            _db.SaveChanges();
+            return Ok("Them thanh cong");
+          }
+          catch{
+             return BadRequest();
+          }
+        }
     }
 
 
